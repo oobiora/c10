8f035d81079642211a3ec0f7fd43bd3aa5a93dc4
protoeloka <oobiora22@gmail.com>
2024-11-04 17:50:11 -0500

    fix for production build

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -4,5 +4,6 @@ export const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: "2024-09-18",
-    useCdn: false
+    useCdn: true,
+    perspective: 'published'
 });
